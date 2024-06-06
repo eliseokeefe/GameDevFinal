df8cd3414c4a351ff92daeabb2608a30248415b7
TANISHJHA <tanishjha05@gmail.com>
2024-06-06 11:52:04 +0200

    Updated shop gui

--- shop_gui.gd
+++ shop_gui.gd
@@ -1,12 +1,11 @@
 extends CanvasLayer
-@onready var open := false 
 signal megaphone 
 signal newspaper 
 signal phone
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	hide()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
