46cdcfc4c38e89a7bebc37ed1d5f5acb1204f694
TANISHJHA <tanishjha05@gmail.com>
2024-06-06 09:36:58 +0200

    Added shop gui script and connected buttons

---
+++ shop_gui.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_megaphone_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_newspaper_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_phone_button_pressed():
+	pass # Replace with function body.
