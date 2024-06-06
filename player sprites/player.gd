43b2927c30de6991ed66c44170620f55740dfc40
eliseokeefe <e.m.okeefe@wustl.edu>
2024-06-06 06:15:05 +0200

    shooting action not working

---
+++ player sprites/player.gd
@@ -0,0 +1,37 @@
+extends CharacterBody2D
+
+var speed = 100 
+var cooldown = true 
+var speech = preload("res://player sprites/speech.tscn")
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction.x == 0 and direction.y == 0:
+		$AnimatedSprite2D.play("idle")
+	elif direction.x < 0:
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = true 
+	else: 
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = false
+	
+	velocity = direction * speed 
+	move_and_slide() 
+	
+	var mouse_pos = get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if Input.is_action_just_pressed("b") and cooldown: 
+		cooldown = false 
+		var speech_instance = speech.instantiate()
+		speech_instance.rotation = $Marker2D.rotation
+		speech_instance.global_position = $Marker2D.global_position
+		add_child(speech_instance)
+		
+		await get_tree().create_timer(0.4).timeout
+		cooldown = true 
+		
+	
+	
+
---
+++ player sprites/speech.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var speed = 300 #can be changed
+
+func _ready():
+	set_as_top_level(true)
+	
+func _process(delta):
+	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
+	
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
