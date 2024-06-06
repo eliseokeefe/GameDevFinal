de8546f4f6d9a435a30c7fb6d1ce2c47c900a170
MoldyBread16 <114259410+MoldyBread16@users.noreply.github.com>
2024-06-06 10:44:31 +0200

    citizen now changes color when hit

--- citizen.gd
+++ citizen.gd
@@ -4,8 +4,17 @@ var rng = RandomNumberGenerator.new()
 var convinced = false
 
 func _on_ready():
-	var speed = randf_range(75.0,150.0)
+	var speed = randf_range(50.0,90.0)
+	print(speed)
 	velocity.x = speed
 
 func _physics_process(delta):
 	move_and_slide()
+
+func change_mind():
+	convinced = true
+	$AnimatedSprite2D.modulate = Color(1, 0, 0)
+
+func _on_hurt_box_body_entered(body):
+	if body.is_in_group("projectiles"):
+		change_mind()
