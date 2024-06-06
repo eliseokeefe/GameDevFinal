d67a8b69990f45a84d081ef52be4693d6d9d79dd
eliseokeefe <e.m.okeefe@wustl.edu>
2024-06-06 18:29:48 +0200

    fixed rotation & refactored powerups' code

--- newspaper.gd
+++ newspaper.gd
@@ -4,7 +4,6 @@ var speed = 300 #can be changed
 var damage = 50 
 var direction: Vector2 
 
-	
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
 
@@ -15,12 +14,6 @@ func _ready():
 	
 func _process(delta):
 	var angle = atan2(direction.y, direction.x)
-	if direction.x > 0:
-		$Sprite2D.flip_h = false 
-		rotation = angle 
-	else: 
-		$Sprite2D.flip_h = true
-		rotation = angle + 2*PI 
-		
 	position += (direction.normalized() *speed) * delta
-	
+	$Sprite2D.flip_h = direction.x < 0 
+
--- player sprites/speech.gd
+++ player sprites/speech.gd
@@ -2,10 +2,8 @@ extends Area2D
 
 var speed = 200 #can be changed
 var damage = 10 
-
 var direction: Vector2 
 
-	
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
 
@@ -16,13 +14,6 @@ func _ready():
 	
 func _process(delta):
 	var angle = atan2(direction.y, direction.x)
-	if direction.x > 0:
-		$Sprite2D.flip_h = false 
-		rotation = angle 
-	else: 
-		$Sprite2D.flip_h = true
-		rotation = angle + 2*PI 
-		
 	position += (direction.normalized() *speed) * delta
-	
+	$Sprite2D.flip_h = direction.x < 0 
 
