14ec72f9e2a36eac0c1f872aaf3908ad7d6712f6
eliseokeefe <e.m.okeefe@wustl.edu>
2024-06-06 13:00:27 +0200

    player/speech update

--- player sprites/player.gd
+++ player sprites/player.gd
@@ -4,6 +4,12 @@ var speed = 100
 var cooldown = true 
 var speech = preload("res://player sprites/speech.tscn")
 
+#var megaphone = preload("res://player sprites/megaphone power up sprite.png")
+#var newspaper = preload("res://player sprites/IMG_8149.PNG")
+#var phone = preload("res://player sprites/IMG_8159.PNG")
+
+var currentSPeech = speech;
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
@@ -20,18 +26,34 @@ func _physics_process(delta):
 	move_and_slide() 
 	
 	var mouse_pos = get_global_mouse_position()
-	$Marker2D.look_at(mouse_pos)
+
 	
 	if Input.is_action_just_pressed("left_mouse") and cooldown: 
+		shoot()
+
+	
+func shoot():
 		cooldown = false 
-		var speech_instance = speech.instantiate()
-		speech_instance.rotation = $Marker2D.rotation
-		speech_instance.global_position = $Marker2D.global_position
-		add_child(speech_instance)
+		var newSpeech = currentSPeech.instantiate()
+		get_parent().add_child(newSpeech)
+		var mouse_pos = get_global_mouse_position()
+		newSpeech.direction = mouse_pos - global_position 
+		newSpeech.global_position = global_position 
+		#newSpeech.global_position = $Marker2D.global_position
 		
 		await get_tree().create_timer(0.4).timeout
 		cooldown = true 
-		
+
+func _on_shop_gui_newspaper():
+	#currentSpeech = newspaper.instantiate() 
+	pass
+
+func _on_shop_gui_phone():
+	#powerup = phone.instantiate() 
+	pass
 	
-	
+func _on_shop_gui_megaphone():
+	pass 
+
+
 
--- player sprites/speech.gd
+++ player sprites/speech.gd
@@ -2,12 +2,26 @@ extends Area2D
 
 var speed = 300 #can be changed
 
+var direction: Vector2 
+
+	
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
+
+ 
 func _ready():
 	add_to_group("projectiles")
 	set_as_top_level(true)
 	
 func _process(delta):
-	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
+	var angle = atan2(direction.y, direction.x)
+	if direction.x > 0:
+		$Sprite2D.flip_h = false 
+		rotation = angle 
+	else: 
+		$Sprite2D.flip_h = true
+		rotation = angle + 2*PI 
+		
+	position += (direction.normalized() *speed) * delta
 	
-func _on_visible_on_screen_enabler_2d_screen_exited():
-	queue_free()
+
