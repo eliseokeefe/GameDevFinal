d9074044b4e8b5e1ac12bb0a98aa40a5478987dd
eliseokeefe <e.m.okeefe@wustl.edu>
2024-06-06 06:32:52 +0200

    fixed speech shooting mechs

--- player sprites/player.gd
+++ player sprites/player.gd
@@ -22,7 +22,7 @@ func _physics_process(delta):
 	var mouse_pos = get_global_mouse_position()
 	$Marker2D.look_at(mouse_pos)
 	
-	if Input.is_action_just_pressed("b") and cooldown: 
+	if Input.is_action_just_pressed("left_mouse") and cooldown: 
 		cooldown = false 
 		var speech_instance = speech.instantiate()
 		speech_instance.rotation = $Marker2D.rotation
