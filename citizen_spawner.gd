1d027afc820a23151a54a49011460e54e19d2b5f
MoldyBread16 <114259410+MoldyBread16@users.noreply.github.com>
2024-06-07 12:47:40 +0200

    citizen spawner changes

--- citizen_spawner.gd
+++ citizen_spawner.gd
@@ -5,11 +5,15 @@ extends Node
 @onready var timer : Timer = $Timer
 var citizen_side = -1
 var rng = RandomNumberGenerator.new()
+var end_game = false
 signal change_score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer.start(spawn_frequency)
+
+func end_game_mode():
+	end_game = true
 
 
 func _on_timer_timeout():
@@ -22,6 +26,8 @@ func _on_timer_timeout():
 	else:
 		instance.go_the_other_direction()
 		instance.position = Vector2(1100,rng.randi_range(-120, 275))
+	if end_game:
+		instance.citizen_evil_now()
 	# restart timer
 	spawn_frequency = rng.randi_range(1, 2)
 	timer.start(spawn_frequency)
