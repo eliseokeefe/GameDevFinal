fee94c585f186de22099647f0b322d588b1ecd90
janewhiite <jaw426@lehigh.edu>
2024-06-07 12:29:42 +0200

    Popup Screen

--- popup.gd
+++ popup.gd
@@ -3,5 +3,10 @@ extends Control
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://part_2.tscn")
 	pass
+
+
+func _on_ready():
+	$Label.text = "Congratulations on convincing " + str(ScoreManager.score) + " people!
+	You now learn your information was incorrect and you need to alert all the people you convinced. Can you revert their opinions?"
