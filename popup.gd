5666c7d5b459f42e58edc7165b90997df90480d0
janewhiite <jaw426@lehigh.edu>
2024-06-07 12:36:21 +0200

    Update popup.gd

--- popup.gd
+++ popup.gd
@@ -8,5 +8,9 @@ func _on_button_pressed():
 
 
 func _on_ready():
-	$Label.text = "Congratulations on convincing " + str(ScoreManager.score) + " people!
-	You now learn your information was incorrect and you need to alert all the people you convinced. Can you revert their opinions?"
+	if (ScoreManager.score == 1):
+		$Label.text = "Congratulations on convincing " + str(ScoreManager.score) + " person!
+		You now learn your information was incorrect and you need to alert all the people you convinced. Can you revert their opinions?"
+	else: 
+		$Label.text = "Congratulations on convincing " + str(ScoreManager.score) + " people!
+		You now learn your information was incorrect and you need to alert all the people you convinced. Can you revert their opinions?"
