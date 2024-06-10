d7ee1db3c6db09980a08249915a90d4a8a366fb1
janewhiite <jaw426@lehigh.edu>
2024-06-10 10:07:41 +0200

    end

--- end.gd
+++ end.gd
@@ -3,5 +3,5 @@ extends Node2D
 @onready var score_label := $ScoreLabel
 
 func _on_ready():
-	$change.text = "You convinced " + str(ScoreManager.score) + " people of your original message, but could only change ____ minds back towards the factual information."
-
+	$change1.text = "JANE" #+ str(ScoreManager.score)
+	$change2.text = "0"
