8e2fa475e7ffe3c2a53b1850a323e456d607e7b7
TANISHJHA <tanishjha05@gmail.com>
2024-06-10 11:58:57 +0200

    Added shop SFX

--- shop_gui.gd
+++ shop_gui.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 signal megaphone 
 signal newspaper 
 signal phone 
-
+@onready var sound = $CashSFX
 @onready var inGameHUD = get_node("/root/main/InGameHUD")
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,7 +18,8 @@ func _on_megaphone_button_pressed():
 	if inGameHUD.money >= 500:
 		$MegaphoneButton.disabled = true  
 		inGameHUD.update_Money(500) 
-		$MoneyLabel.text = "Donations: " + str(inGameHUD.money) 
+		$MoneyLabel.text = "Donations: " + str(inGameHUD.money)  
+		sound.play()
 		megaphone.emit()
 	
 
@@ -27,7 +28,8 @@ func _on_newspaper_button_pressed():
 	if inGameHUD.money >= 1500:
 		$NewspaperButton.disabled = true  
 		inGameHUD.update_Money(1500)
-		$MoneyLabel.text = "Donations: " + str(inGameHUD.money)
+		$MoneyLabel.text = "Donations: " + str(inGameHUD.money) 
+		sound.play()
 		newspaper.emit()
 
 
@@ -35,7 +37,8 @@ func _on_phone_button_pressed():
 	if inGameHUD.money >= 3000:	
 		$PhoneButton.disabled = true  
 		inGameHUD.update_Money(3000)
-		$MoneyLabel.text = "Donations: " + str(inGameHUD.money)
+		$MoneyLabel.text = "Donations: " + str(inGameHUD.money) 
+		sound.play()
 		phone.emit()
 
 
