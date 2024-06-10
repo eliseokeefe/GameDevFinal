ea291036a4e08f69dc7d3c696c6045247e097a14
eliseokeefe <e.m.okeefe@wustl.edu>
2024-06-10 13:25:44 +0200

    button for end screen/background

--- end.gd
+++ end.gd
@@ -5,5 +5,9 @@ extends Node2D
 #@onready var score_label := $ScoreLabel
 
 func _ready():
+	$Button.visible = false 
 	$change1.text = str(ScoreManager.get_first_score()/100)
 	$change2.text = str(ScoreManager.get_second_score()/100)
+	await get_tree().create_timer(3).timeout
+	$Button.visible = true 
+	
